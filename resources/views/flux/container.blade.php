296f683e3be8dcf93e5d1d51600b12e7e90e09e9
wijayy <mertawijaya77@gmail.com>
2026-01-31 15:06:35 +0800

    change build

--- resources/views/flux/container.blade.php
+++ resources/views/flux/container.blade.php
@@ -1,7 +1,5 @@
 @php
-$classes = Flux::classes()
-    ->add('mx-auto w-full [:where(&)]:max-w-7xl')
-    ;
+    $classes = Flux::classes()->add('mx-auto p-2 md:p-0 w-full [:where(&)]:max-w-7xl');
 @endphp
 
 <div {{ $attributes->class($classes) }} data-flux-container>
